e1e4e1f85a997de96a262daa02e5fc7cc3f1567d
Shardul Chiplunkar <shardul.chiplunkar@epfl.ch>
2022-11-01 11:56:50 +0100

    oops forgot Interpreter.scala

---
+++ src/main/scala/tastyinterpreter/Interpreter.scala
@@ -0,0 +1,19 @@
+package tastyinterpreter
+
+import tastyquery.Contexts.*
+import tastyquery.Names.FullyQualifiedName
+import tastyquery.Trees.Tree
+
+class Interpreter(using Context):
+
+  val topLevelEnv = ScalaEnvironment(None)
+  val ctx = tastyquery.Contexts.ctx
+
+  def evaluate(tree: Tree) =
+    Evaluators.evaluate(topLevelEnv)(tree)
+
+  def evaluateDeclarationsInPackage(packageName: FullyQualifiedName) =
+    ctx.findPackageFromRoot(packageName).asPackage.declarations
+      // asInstanceOf is safe because all DefTree subclasses are Tree subclasses
+      .map(_.tree.get.asInstanceOf[Tree])
+      .foreach(evaluate)
